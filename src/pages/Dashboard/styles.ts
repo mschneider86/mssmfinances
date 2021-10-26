11b2ffc917e322bcab1a2c0e17991f65536a6d88
Matheus Schneider Silva Mayrink <matheus.mayrink@prodemge.gov.br>
2021-10-26 10:48:21 -0300

    added app theme

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,9 +1,16 @@
 import styled from 'styled-components/native';
+import theme from '../../global/styles/theme';
 
 export const Container = styled.View`
+  flex: 1;
   justify-content: center;
   align-items: center;
-  flex: 1;
+
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.title};
+`;
