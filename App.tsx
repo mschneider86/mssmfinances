126ae45e1b1a7d8bc224fbaa9150ac5a7255e62d
Matheus Schneider Silva Mayrink <matheus.mayrink@prodemge.gov.br>
2021-10-28 15:53:21 -0300

    added tab navigation

--- App.tsx
+++ App.tsx
@@ -12,7 +12,8 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/pages/Dashboard';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +28,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
