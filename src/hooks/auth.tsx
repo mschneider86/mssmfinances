9f3df8c828d164209779d894660354dd98904f66
Matheus Schneider Silva Mayrink <matheus.mayrink@prodemge.gov.br>
2021-11-09 14:58:07 -0300

    added user name & photo to dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -109,11 +109,14 @@ function AuthProvider({ children }: AuthContextProps) {
       });
 
       if (credential) {
+        const name = credential.fullName!.givenName!;
+        const photo = `https://ui-avatars.com/api/?name=${name}&length=1`;
+
         const userLogged = {
           id: String(credential.user),
           email: credential.email!,
-          name: credential.fullName!.givenName!,
-          photo: undefined,
+          name,
+          photo,
         };
 
         setUser(userLogged);
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -57,7 +57,7 @@ export function Dashboard() {
   );
 
   const theme = useTheme();
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
 
   function getLastTransactionDate(
     collection: DataListProps[],
@@ -177,12 +177,12 @@ export function Dashboard() {
               <UserInfo>
                 <Photo
                   source={{
-                    uri: 'https://avatars.githubusercontent.com/u/26752887?v=4',
+                    uri: user.photo,
                   }}
                 />
                 <User>
                   <UserGreeting>Olá,</UserGreeting>
-                  <UserName>Matheus</UserName>
+                  <UserName>{user.name}</UserName>
                 </User>
               </UserInfo>
 
